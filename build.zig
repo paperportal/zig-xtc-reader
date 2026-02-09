f0a4753ec73e99b2e341bc4c93b92398d6ba5a05
Mika Ryynänen <mikar@me.com>
2026-02-09 12:48:50 +0200

    Build updates

--- build.zig
+++ build.zig
@@ -1,34 +1,20 @@
 const std = @import("std");
-const ppsdk = @import("paper_portal_sdk");
+const sdk = @import("paper_portal_sdk");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{
-        .default_target = .{ .cpu_arch = .wasm32, .os_tag = .freestanding },
+    const app = sdk.addPortalApp(b, .{
+        .local_sdk_path = "../zig-sdk",
+        .export_symbol_names = &.{
+            "pp_contract_version",
+            "pp_init",
+            "pp_tick",
+            "pp_alloc",
+            "pp_free",
+            "pp_on_gesture",
+        },
     });
-    const host_optimize = b.standardOptimizeOption(.{});
-    const optimize = std.builtin.OptimizeMode.ReleaseSmall;
 
-    const root_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-        .strip = true,
-    });
-    root_mod.export_symbol_names = &.{
-        "pp_contract_version",
-        "pp_init",
-        "pp_tick",
-        "pp_alloc",
-        "pp_free",
-        "pp_on_gesture",
-    };
-
-    const exe = b.addExecutable(.{ .name = "main", .root_module = root_mod });
-    exe.entry = .disabled;
-
-    _ = ppsdk.addWasmUpload(b, exe, .{});
-
-    _ = ppsdk.addWasmPortalPackage(b, exe, .{
+    _ = sdk.addPortalPackage(b, app.exe, .{
         .manifest = .{
             .id = "bd55bbdc-41ec-497a-a02f-f3a1ba25dac0",
             .name = "XTC Reader",
@@ -36,30 +22,11 @@ pub fn build(b: *std.Build) void {
         },
     });
 
-    // Prefer to use local Paper Portal SDK if it exists.
-    const sdk_dep = if (dirExists(b, "../zig-sdk"))
-        (b.lazyDependency("paper_portal_sdk_local", .{}) orelse @panic("paper_portal_sdk_local missing"))
-    else
-        b.dependency("paper_portal_sdk", .{});
-
-    const sdk = b.createModule(.{
-        .root_source_file = sdk_dep.path("sdk.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    exe.root_module.addImport("paper_portal_sdk", sdk);
-
-    exe.stack_size = 32 * 1024;
-    exe.initial_memory = 512 * 1024;
-    exe.max_memory = 1024 * 1024;
-
-    b.installArtifact(exe);
-
     // CLI tool runnable on host computer for examining xtc files.
     const xtci_mod = b.createModule(.{
         .root_source_file = b.path("src/main_xtci.zig"),
         .target = b.graph.host,
-        .optimize = host_optimize,
+        .optimize = .ReleaseFast,
         .strip = false,
     });
     const xtci_exe = b.addExecutable(.{
